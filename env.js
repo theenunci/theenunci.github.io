ee7890c09076d6a3f1d0cb4cc45e62f6fd5a960d
theenunci <enunci.work@gmail.com>
2023-05-11 20:31:10 +0700

    init

---
+++ env.js
@@ -0,0 +1,10 @@
+/*
+ * Define default environment variables of the application.
+ * You can customize these values by overriding them to public/configs/env-local.js (create this file if not exist).
+ * Don't forget to uncomment the env-local.js <script> tag in index.html.
+ * To enable tracking, replace tracker.appId with your app ID in env-local.js.
+ */
+
+window.config = {
+  requestURL: 'http://localhost:8000'
+};
