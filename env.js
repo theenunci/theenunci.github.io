32778d150358856a0cd9c8626a9eea2a899eb019
iHateEggs <daikia17@gmail.com>
2023-06-27 15:58:54 +0700

    add to deploy

--- env.js
+++ env.js
@@ -3,6 +3,7 @@
  * You can customize these values by overriding them to public/configs/env-local.js (create this file if not exist).
  * Don't forget to uncomment the env-local.js <script> tag in index.html.
  * To enable tracking, replace tracker.appId with your app ID in env-local.js.
+ * Add the line for testing deploy
  */
 
 window.config = {
